51e25e5bff61e29448bc9c8fecffcfc9ec9cb177
mhdahdalw <mhdahdalw@gmail.com>
2025-12-20 18:14:25 +0330

    my lazyVim config

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = 40,
+    --open_mapping = [[[<leader><C-t>]], -- same key LazyVim uses for its built‑in terminal
+    shade_terminals = true,
+    direction = "vertical", -- "horizontal", -- change to "vertical", "float", or "tab"
+    start_in_insert = true,
+  },
+}
